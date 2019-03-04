396c6af2932a6e5ff5e24c9d1e3714787293e81f
Kang Cai <caikangpku@gmail.com>
2019-03-04 09:25:01 +0800

    Update lr.py

--- code/lr.py
+++ code/lr.py
@@ -2,3 +2,17 @@
 
 from numpy import *
 
+class LogisticRegressionModel(object):
+    """
+    罗吉斯蒂克回归
+    """
+    def __init__(self):
+        pass
+
+    def train(self, data):
+        """
+        训练模型
+        :param data: 以 [[label] [input_text_words]] 的形式构成的list
+        :return: None
+        """
+
