72ebce48d7d8e7a22b709013b1988e8171e863dd
Kang Cai <caikangpku@gmail.com>
2019-04-18 10:35:04 +0800

    self

---
+++ code/models_with_sklearn.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+import sklearn
+
+nb = sklearn.naive_bayes.BernoulliNB()
+lr = sklearn.linear_model.LogisticRegression()
+svm_model = sklearn.svm.SVC()
+
+
---
+++ code/svm.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import time
+
+class SVMModel(object):
+    """
+    SVM模型
+    """
+    def __init__(self, kernel):
+        self.kernel = kernel
+        self.C = None
+
+    def train(self, x_train, y_train):
+        """
+        模型训练
+        :param x_train: shape = num_train, dim_feature
+        :param y_train: shape = num_train, 1
+        :return: loss_history
+        """
+        num_train, dim_feature = x_train.shape
+
+
